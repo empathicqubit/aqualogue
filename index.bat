82cc18098ca908d6546c70036e485a449bd42e0c
RedEnchilada <red@lyrawearspants.com>
2016-05-13 20:39:02 -0500

    Add styling to the page around the game

--- index.bat
+++ index.bat
@@ -1,4 +1,5 @@
 echo ^<html^>^<body^> > index.html
+copy /y header.html index.html
 dir *.js /s /b > tmpfile
 for /F "tokens=*" %%A in (tmpfile) do (
 	echo ^<script type="text/javascript" src="file:///%%A"^>^</script^> >> index.html
